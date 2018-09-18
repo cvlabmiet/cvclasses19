841ce68eb5d77f2da0d4161c4525f84a8a72c47f
KonochkinKN <AWESOME.KOSTYAN@Ya.ru>
2018-09-18 21:45:16 +0300

    Replaced tabs with spaces

--- cvlib/tests/split_and_merge.cpp
+++ cvlib/tests/split_and_merge.cpp
@@ -44,15 +44,15 @@ TEST_CASE("simple regions", "[split_and_merge]")
 
     SECTION("3x3")
     {
-		const cv::Mat reference = (cv::Mat_<char>(3, 3) << 4, 4, 4, 4, 4, 4, 4, 4, 4);
-		cv::Mat image = (cv::Mat_<char>(3, 3) << 0, 1, 2, 3, 4, 5, 6, 7, 8);
-		auto res = split_and_merge(image, 10, 10);
-		REQUIRE(image.size() == res.size());
-		REQUIRE(image.type() == res.type());
-		REQUIRE(0 == cv::countNonZero(reference - res));
+        const cv::Mat reference = (cv::Mat_<char>(3, 3) << 4, 4, 4, 4, 4, 4, 4, 4, 4);
+        cv::Mat image = (cv::Mat_<char>(3, 3) << 0, 1, 2, 3, 4, 5, 6, 7, 8);
+        auto res = split_and_merge(image, 10, 10);
+        REQUIRE(image.size() == res.size());
+        REQUIRE(image.type() == res.type());
+        REQUIRE(0 == cv::countNonZero(reference - res));
 
-		res = split_and_merge(image, 1, 10);
-		REQUIRE(0 == cv::countNonZero(image - res));
+        res = split_and_merge(image, 1, 10);
+        REQUIRE(0 == cv::countNonZero(image - res));
     }
 }
 
@@ -60,40 +60,40 @@ TEST_CASE("compex regions", "[split_and_merge]")
 {
     SECTION("2x2")
     {
-		const cv::Mat reference = (cv::Mat_<char>(2, 2) << 2, 2, 37, 37);
-		cv::Mat image = (cv::Mat_<char>(2, 2) << 1, 3, 42, 32);
-		auto res = split_and_merge(image, 10, 10);
-		REQUIRE(image.size() == res.size());
-		REQUIRE(image.type() == res.type());
-		REQUIRE(0 == cv::countNonZero(reference - res));
+        const cv::Mat reference = (cv::Mat_<char>(2, 2) << 2, 2, 37, 37);
+        cv::Mat image = (cv::Mat_<char>(2, 2) << 1, 3, 42, 32);
+        auto res = split_and_merge(image, 10, 10);
+        REQUIRE(image.size() == res.size());
+        REQUIRE(image.type() == res.type());
+        REQUIRE(0 == cv::countNonZero(reference - res));
 
-		res = split_and_merge(image, 1, 10);
-		REQUIRE(0 == cv::countNonZero(image - res));
+        res = split_and_merge(image, 1, 10);
+        REQUIRE(0 == cv::countNonZero(image - res));
     }
 
     SECTION("3x3")
     {
-		const cv::Mat reference = (cv::Mat_<char>(3, 3) << 2, 2, 2, 70, 15, 15, 70, 15, 15);
-		cv::Mat image = (cv::Mat_<char>(3, 3) << 1, 2, 3, 69, 15, 16, 71, 14, 15);
-		auto res = split_and_merge(image, 10, 10);
-		REQUIRE(image.size() == res.size());
-		REQUIRE(image.type() == res.type());
-		REQUIRE(0 == cv::countNonZero(reference - res));
+        const cv::Mat reference = (cv::Mat_<char>(3, 3) << 2, 2, 2, 70, 15, 15, 70, 15, 15);
+        cv::Mat image = (cv::Mat_<char>(3, 3) << 1, 2, 3, 69, 15, 16, 71, 14, 15);
+        auto res = split_and_merge(image, 10, 10);
+        REQUIRE(image.size() == res.size());
+        REQUIRE(image.type() == res.type());
+        REQUIRE(0 == cv::countNonZero(reference - res));
 
-		res = split_and_merge(image, 1, 10);
-		REQUIRE(0 == cv::countNonZero(image - res));
+        res = split_and_merge(image, 1, 10);
+        REQUIRE(0 == cv::countNonZero(image - res));
     }
 
     SECTION("4x4")
     {
-		const cv::Mat reference = (cv::Mat_<char>(4, 4) << 3, 3, 3, 3,  3, 3, 3, 3,  20, 20, 20, 20,  40, 40, 40, 40);
-		cv::Mat image = (cv::Mat_<char>(4, 4) << 1, 2, 3, 2,   4, 5, 3, 4,   21, 19, 22, 18,   41, 39, 40, 40);
-		auto res = split_and_merge(image, 10, 10);
-		REQUIRE(image.size() == res.size());
-		REQUIRE(image.type() == res.type());
-		REQUIRE(0 == cv::countNonZero(reference - res));
+        const cv::Mat reference = (cv::Mat_<char>(4, 4) << 3, 3, 3, 3,  3, 3, 3, 3,  20, 20, 20, 20,  40, 40, 40, 40);
+        cv::Mat image = (cv::Mat_<char>(4, 4) << 1, 2, 3, 2,   4, 5, 3, 4,   21, 19, 22, 18,   41, 39, 40, 40);
+        auto res = split_and_merge(image, 10, 10);
+        REQUIRE(image.size() == res.size());
+        REQUIRE(image.type() == res.type());
+        REQUIRE(0 == cv::countNonZero(reference - res));
 
-		res = split_and_merge(image, 1, 10);
-		REQUIRE(0 == cv::countNonZero(image - res));
+        res = split_and_merge(image, 1, 10);
+        REQUIRE(0 == cv::countNonZero(image - res));
     }
 }
--- demo/main.cpp
+++ demo/main.cpp
@@ -15,44 +15,44 @@ int main(int argc, char* argv[])
 
     cv::Mat frame;
     cv::Mat frame4split;
-	cv::Mat frame4split_and_merge;
+    cv::Mat frame4split_and_merge;
 
     const auto origin_wnd = "origin";
     const auto demo_wnd = "demo";
-	const auto split_wnd = "split";
+    const auto split_wnd = "split";
 
     int stddev = 20;
-	int diffmean = 10;
+    int diffmean = 10;
     cv::namedWindow(demo_wnd, 1);
-	cv::namedWindow(split_wnd, 1);
+    cv::namedWindow(split_wnd, 1);
     cv::createTrackbar("stdev", split_wnd, &stddev, 100);
-	cv::createTrackbar("diffmean", demo_wnd, &diffmean, 100);
+    cv::createTrackbar("diffmean", demo_wnd, &diffmean, 100);
 
     while (cv::waitKey(30) != 27) // ESC
     {
         cap >> frame;
 
-		std::vector <cv::Mat> rgb4split;
-		std::vector <cv::Mat> rgb4split_and_merge;
-		cv::split(frame, rgb4split);
-		cv::split(frame, rgb4split_and_merge);
+        std::vector <cv::Mat> rgb4split;
+        std::vector <cv::Mat> rgb4split_and_merge;
+        cv::split(frame, rgb4split);
+        cv::split(frame, rgb4split_and_merge);
 
-		cvlib::split_only(rgb4split[0], stddev);
-		cvlib::split_only(rgb4split[1], stddev);
-		cvlib::split_only(rgb4split[2], stddev);
+        cvlib::split_only(rgb4split[0], stddev);
+        cvlib::split_only(rgb4split[1], stddev);
+        cvlib::split_only(rgb4split[2], stddev);
 
-		cvlib::split_and_merge(rgb4split_and_merge[0], stddev, diffmean);
-		cvlib::split_and_merge(rgb4split_and_merge[1], stddev, diffmean);
-		cvlib::split_and_merge(rgb4split_and_merge[2], stddev, diffmean);
+        cvlib::split_and_merge(rgb4split_and_merge[0], stddev, diffmean);
+        cvlib::split_and_merge(rgb4split_and_merge[1], stddev, diffmean);
+        cvlib::split_and_merge(rgb4split_and_merge[2], stddev, diffmean);
 
-		cv::merge(rgb4split, frame4split);
-		cv::merge(rgb4split_and_merge, frame4split_and_merge);
+        cv::merge(rgb4split, frame4split);
+        cv::merge(rgb4split_and_merge, frame4split_and_merge);
 
         //cv::cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
-		//cv::cvtColor(frame, frame_gray4split, cv::COLOR_BGR2GRAY);
+        //cv::cvtColor(frame, frame_gray4split, cv::COLOR_BGR2GRAY);
         cv::imshow(origin_wnd, frame);
         cv::imshow(demo_wnd, frame4split_and_merge);//cvlib::split_and_merge(frame_gray, stddev, diffmean));
-		cv::imshow(split_wnd, frame4split);//cvlib::split_only(frame_gray4split, stddev));
+        cv::imshow(split_wnd, frame4split);//cvlib::split_only(frame_gray4split, stddev));
     }
 
     return 0;
