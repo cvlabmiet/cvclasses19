cb1f6341fdec5fdbe1deec9dc5c5cc8d8af7ef30
Dmitrij Vukalov <dmitrij.vukalov@yandex.ru>
2018-09-18 21:49:56 +0300

    Code style

--- cvlib/src/split_and_merge.cpp
+++ cvlib/src/split_and_merge.cpp
@@ -63,9 +63,9 @@ void merge_image(cv::Mat image, double stddev)
 
     cv::Mat R_1 = image(cv::Range(0, height / 2), cv::Range(width / 2, width));
     
-	cv::Mat R_2 = image(cv::Range(height / 2, height), cv::Range(0, width / 2));
+    cv::Mat R_2 = image(cv::Range(height / 2, height), cv::Range(0, width / 2));
 
-	cv::Mat R_3 = image(cv::Range(height / 2, height), cv::Range(width / 2, width));
+    cv::Mat R_3 = image(cv::Range(height / 2, height), cv::Range(width / 2, width));
     
     
     cv::meanStdDev(R_0, Rmean_0, Rdev_0);
@@ -78,48 +78,48 @@ void merge_image(cv::Mat image, double stddev)
     int flag_2 = 0;
     int flag_3 = 0;
     if (Rdev_0.at<double>(0) <= stddev && Rdev_1.at<double>(0) <= stddev &&
-		abs(Rmean_0.at<double>(0) - Rmean_1.at<double>(0)) <= diffMean)
+        abs(Rmean_0.at<double>(0) - Rmean_1.at<double>(0)) <= diffMean)
     {
         flag_0 = 1;
         R_0.setTo((Rmean_0 + Rmean_1) / 2);
         R_1.setTo((Rmean_0 + Rmean_1) / 2);
     }
 
-	if (Rdev_3.at<double>(0) <= stddev && Rdev_1.at<double>(0) <= stddev && !flag_0 &&
-		abs(Rmean_3.at<double>(0) - Rmean_1.at<double>(0)) <= diffMean)
-	{
-		flag_2 = 1;
-		R_3.setTo((Rmean_3 + Rmean_1) / 2);
-		R_1.setTo((Rmean_3 + Rmean_1) / 2);
-	}
+    if (Rdev_3.at<double>(0) <= stddev && Rdev_1.at<double>(0) <= stddev && !flag_0 &&
+        abs(Rmean_3.at<double>(0) - Rmean_1.at<double>(0)) <= diffMean)
+    {
+        flag_2 = 1;
+        R_3.setTo((Rmean_3 + Rmean_1) / 2);
+        R_1.setTo((Rmean_3 + Rmean_1) / 2);
+    }
 
-	if (Rdev_3.at<double>(0) <= stddev && Rdev_2.at<double>(0) <= stddev && !flag_2 &&
-		abs(Rmean_3.at<double>(0) - Rmean_2.at<double>(0)) <= diffMean)
-	{
-		flag_3 = 1;
-		R_2.setTo((Rmean_3 + Rmean_2) / 2);
-		R_3.setTo((Rmean_3 + Rmean_2) / 2);
-	}
+    if (Rdev_3.at<double>(0) <= stddev && Rdev_2.at<double>(0) <= stddev && !flag_2 &&
+        abs(Rmean_3.at<double>(0) - Rmean_2.at<double>(0)) <= diffMean)
+    {
+        flag_3 = 1;
+        R_2.setTo((Rmean_3 + Rmean_2) / 2);
+        R_3.setTo((Rmean_3 + Rmean_2) / 2);
+    }
 
     if (Rdev_0.at<double>(0) <= stddev && Rdev_2.at<double>(0) <= stddev && !flag_0 && !flag_3 &&
-		abs(Rmean_0.at<double>(0) - Rmean_2.at<double>(0)) <= diffMean)
+        abs(Rmean_0.at<double>(0) - Rmean_2.at<double>(0)) <= diffMean)
     {
         flag_1 = 1;
         R_0.setTo((Rmean_0 + Rmean_2) / 2);
         R_2.setTo((Rmean_0 + Rmean_2) / 2);
     }
 
-	if (!flag_0 && !flag_1)
-		merge_image(R_0, stddev);
+    if (!flag_0 && !flag_1)
+        merge_image(R_0, stddev);
 
-	if (!flag_0 && !flag_2) 
-		merge_image(R_1, stddev);
+    if (!flag_0 && !flag_2) 
+        merge_image(R_1, stddev);
 
-	if (!flag_1 && !flag_3) 
-		merge_image(R_2, stddev);
+    if (!flag_1 && !flag_3) 
+        merge_image(R_2, stddev);
 
-	if (!flag_2 && !flag_3) 
-		merge_image(R_3, stddev);
+    if (!flag_2 && !flag_3) 
+        merge_image(R_3, stddev);
 
 }
 }
@@ -142,19 +142,19 @@ cv::Mat split_and_merge(const cv::Mat& image, double stddev)
 
 namespace cvlib
 {
-	cv::Mat cvlib_split(const cv::Mat& image, double stddev)
-	{
-		cv::Mat res = image;
-		split_image(res, stddev);
-		return res;
-	}
+    cv::Mat cvlib_split(const cv::Mat& image, double stddev)
+    {
+        cv::Mat res = image;
+        split_image(res, stddev);
+        return res;
+    }
 }
 
 namespace cvlib
 {
 cv::Mat cvlib_merge(const cv::Mat& image, double stddev)
 {
-	cv::Mat res = image;
+    cv::Mat res = image;
     merge_image(res, stddev);
     return res;
 }
--- demo/main.cpp
+++ demo/main.cpp
@@ -6,6 +6,7 @@
 
 #include <cvlib.hpp>
 #include <opencv2/opencv.hpp>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
@@ -14,23 +15,46 @@ int main(int argc, char* argv[])
         return -1;
 
     cv::Mat frame;
-    cv::Mat frame_gray;
+    cv::Mat frame_gray1;
+    cv::Mat frame_gray2;
+
 
     const auto origin_wnd = "origin";
-    const auto demo_wnd = "demo";
+    const auto split_wnd = "split";
+    const auto merge_wnd = "merge";
 
     int stddev = 50;
-    cv::namedWindow(demo_wnd, 1);
+    cv::namedWindow(split_wnd, 1);
+    cv::namedWindow(merge_wnd, 1);
+
+
     // \todo choose reasonable max value
-    cv::createTrackbar("stdev", demo_wnd, &stddev, 255);
+    cv::createTrackbar("stdev", split_wnd, &stddev, 255);
 
     while (cv::waitKey(30) != 27) // ESC
     {
         cap >> frame;
 
-        cv::cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
+        std::vector <cv::Mat> rgb;
+        cv::cvtColor(frame, frame_gray1, cv::COLOR_BGR2GRAY);
+        frame_gray2 = frame_gray1.clone();
+        //cv::cvtColor(frame, frame_gray2, cv::COLOR_BGR2GRAY);
+
+        //cv::split(frame, rgb);
+
+        
+        //cvlib::test_merge(rgb[0], stddev);
+        //cvlib::test_merge(rgb[1], stddev);
+        //cvlib::test_merge(rgb[2], stddev);
+        //cvlib::split_and_merge(rgb[0], stddev);
+        //cvlib::split_and_merge(rgb[1], stddev);
+        //cvlib::split_and_merge(rgb[2], stddev);
+
+        //cv:merge(rgb, frame_gray);
+        
         cv::imshow(origin_wnd, frame);
-        cv::imshow(demo_wnd, cvlib::split_and_merge(frame_gray, stddev));
+        cv::imshow(split_wnd, cvlib::cvlib_split(frame_gray1, stddev));
+        cv::imshow(merge_wnd, cvlib::cvlib_merge(frame_gray2, stddev));
     }
 
     return 0;
