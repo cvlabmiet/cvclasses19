99979e7da85cb13e29318bb41595589df519603d
KonochkinKN <AWESOME.KOSTYAN@Ya.ru>
2018-09-23 22:50:12 +0300

    Stable version of texture segmentation

--- cvlib/src/select_texture.cpp
+++ cvlib/src/select_texture.cpp
@@ -9,8 +9,9 @@
 namespace
 {
 
-cv::Mat gGaborKernels[3];
 int gKernelSize = 0;
+cv::Mat gGaborKernels[3];
+cv::Mat gFilteredImages[3];
 
 void getKernel(int kernel_size)
 {
@@ -21,9 +22,58 @@ void getKernel(int kernel_size)
 
     for (int i = 0; i < 3; i++)
     {
+        gGaborKernels[i].release();
         gGaborKernels[i] = cv::getGaborKernel(cv::Size(kernel_size, kernel_size), sig[i], th, lm, gm);
     }
 }
+
+void filterImage(const cv::Mat& image)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        gFilteredImages[i].release();
+        cv::filter2D(image, gFilteredImages[i], CV_32F, gGaborKernels[i]);
+    }
+}
+struct Descriptor
+{
+    Descriptor() // ctor
+    {
+        this->clear();
+    }
+
+    Descriptor operator-(const Descriptor& right) const
+    {
+        Descriptor temp = *this;
+        for (int i = 0; i < 6; ++i)
+        {
+            temp.m_descriptor[i] -= right.m_descriptor[i];
+        }
+        return temp;
+    }
+
+    void clear()
+    {
+        m_descriptor[0] = 0;
+        m_descriptor[1] = 0;
+        m_descriptor[2] = 0;
+        m_descriptor[3] = 0;
+        m_descriptor[4] = 0;
+        m_descriptor[5] = 0;
+    }
+
+    double norm_l2() const
+    {
+        double res = 0.0;
+        for (int i = 0; i < 6; ++i)
+        {
+            res += m_descriptor[i]* m_descriptor[i];
+        }
+        return std::sqrt(res);
+    }
+
+    double m_descriptor[6];
+};
 
 struct descriptor : public std::vector<double>
 {
@@ -65,13 +115,31 @@ struct descriptor : public std::vector<double>
     }
 };
 
-void calculateDescriptor(const cv::Mat& image,/* int kernel_size,*/ descriptor& descr)
+void calculateDescriptorForPatch(const cv::Mat& image, descriptor& descr)
 {
     descr.clear();
-    const double th = CV_PI / 4;
-    const double lm = 10.0;
-    const double gm = 0.5;
     cv::Mat response;
+    cv::Mat mean;
+    cv::Mat dev;
+
+    for (int i = 0; i < 3; i++)
+    {
+        cv::filter2D(image, response, CV_32F, gGaborKernels[i]);
+        cv::meanStdDev(response, mean, dev);
+        //descr.m_descriptor[2 * i] = mean.at<double>(0);
+        //descr.m_descriptor[2 * i + 1] = mean.at<double>(0);
+        descr.emplace_back(mean.at<double>(0));
+        descr.emplace_back(dev.at<double>(0));
+    }
+}
+
+void calculateDescriptor(const cv::Rect& roi,/*const cv::Mat& image,*//* int kernel_size,*/ descriptor& descr)
+{
+    descr.clear();
+    //const double th = CV_PI / 4;
+    //const double lm = 10.0;
+    //const double gm = 0.5;
+    //cv::Mat response;
     cv::Mat mean;
     cv::Mat dev;
 
@@ -82,10 +150,11 @@ void calculateDescriptor(const cv::Mat& image,/* int kernel_size,*/ descriptor& 
     {
         //cv::Mat kernel = cv::getGaborKernel(cv::Size(kernel_size, kernel_size), sig, th, lm, gm);
         //cv::filter2D(image, response, CV_32F, kernel);
-        cv::filter2D(image, response, CV_32F, gGaborKernels[i]);
-        cv::meanStdDev(response, mean, dev);
-        //descr[2 * i] = mean.at<double>(0);
-        //descr[2 * i + 1] = mean.at<double>(0);
+        //cv::filter2D(image, response, CV_32F, gGaborKernels[i]);
+        //cv::meanStdDev(response, mean, dev);
+        cv::meanStdDev(gFilteredImages[i](roi), mean, dev);
+        //descr.m_descriptor[2 * i] = mean.at<double>(0);
+        //descr.m_descriptor[2 * i + 1] = mean.at<double>(0);
         descr.emplace_back(mean.at<double>(0));
         descr.emplace_back(dev.at<double>(0));
     }
@@ -105,23 +174,27 @@ cv::Mat select_texture(const cv::Mat& image, const cv::Rect& roi, double eps, bo
     }
 
     descriptor reference;
+    //Descriptor reference;
     //calculateDescriptor(image(roi), kernel_size, reference);
-    calculateDescriptor(image(roi), reference);
+    calculateDescriptorForPatch(image(roi), reference);
 
     cv::Mat res = cv::Mat::zeros(image.size(), CV_8UC1);
 
     descriptor test(reference.size());
+    //Descriptor test;
     cv::Rect baseROI = roi - roi.tl();
+
+    filterImage(image);
 
     // \todo move ROI smoothly pixel-by-pixel
     for (int i = 0; i < image.size().width - roi.width; ++i)
     {
         for (int j = 0; j < image.size().height - roi.height; ++j)
         {
-            std::cout << "col: " << i << "\trow: " << j << std::endl;
             auto curROI = baseROI + cv::Point(/*roi.width **/ i, /*roi.height **/ j);
             //calculateDescriptor(image(curROI), kernel_size, test);
-            calculateDescriptor(image(curROI), test);
+            //calculateDescriptor(image(curROI), test);
+            calculateDescriptor(curROI, test);
 
             res(curROI) = 255 * ((test - reference).norm_l2() <= eps);
         }
