6ae158776f85e43646eff0e404227b1addbb1acc
Roman Golovanov <golovanov@org.miet.ru>
2018-10-16 10:11:19 +0300

    Add lab4 blank code

--- cvlib/include/cvlib.hpp
+++ cvlib/include/cvlib.hpp
@@ -43,6 +43,18 @@ class motion_segmentation : public cv::BackgroundSubtractor
     private:
     cv::Mat bg_model_;
 };
+
+/// \brief FAST corner detection algorithm
+class corner_detector_fast : public cv::Feature2D
+{
+public:
+   /// \brief Fabrique method for creating FAST detector
+   static cv::Ptr<corner_detector_fast> create();
+
+   /// \see Feature2d::detect
+   virtual void detect(cv::InputArray image, CV_OUT std::vector<cv::KeyPoint>& keypoints,
+      cv::InputArray mask = cv::noArray()) override;
+};
 } // namespace cvlib
 
 #endif // __CVLIB_HPP__
---
+++ demo/demo_corner_detector.cpp
@@ -0,0 +1,39 @@
+/* Demo application for Computer Vision Library.
+* @file
+* @date 2018-09-05
+* @author Anonymous
+*/
+
+#include <cvlib.hpp>
+#include <opencv2/opencv.hpp>
+
+int demo_corner_detector(int argc, char* argv[])
+{
+   cv::VideoCapture cap(0);
+   if (!cap.isOpened())
+      return -1;
+
+   const auto main_wnd = "main";
+   const auto demo_wnd = "demo";
+
+   cv::namedWindow(main_wnd);
+   cv::namedWindow(demo_wnd);
+
+   cv::Mat frame;
+   auto detector = cv::GFTTDetector::create(); // \todo use cvlib::corner_detector_fast
+   std::vector<cv::KeyPoint> corners;
+   while (cv::waitKey(30) != 27) // ESC
+   {
+      cap >> frame;
+      cv::imshow(main_wnd, frame);
+
+      detector->detect(frame, corners);
+      cv::drawKeypoints(frame, corners, frame, cv::Scalar(0, 0, 255));
+      cv::imshow(demo_wnd, frame);
+   }
+
+   cv::destroyWindow(main_wnd);
+   cv::destroyWindow(demo_wnd);
+
+   return 0;
+}
