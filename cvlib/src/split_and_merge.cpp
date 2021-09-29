08b5407c121c777efcbbb8d4d79897c8a3512afb
SeregaVM <studentvm2019@gmail.com>
2021-09-29 23:07:11 +0300

    lab1 'Split and merge' done

--- cvlib/src/split_and_merge.cpp
+++ cvlib/src/split_and_merge.cpp
@@ -5,41 +5,192 @@
  */
 
 #include "cvlib.hpp"
+#include <vector>
+#include <list>
 
-namespace
+namespace SaM
 {
-void split_image(cv::Mat image, double stddev)
-{
-    cv::Mat mean;
-    cv::Mat dev;
-    cv::meanStdDev(image, mean, dev);
-
-    if (dev.at<double>(0) <= stddev)
+    struct RoI
     {
-        image.setTo(mean);
-        return;
+        std::vector<cv::Rect> region_parts;
+        int x, y, w, h;
+        int accum_mean = 0;
+        int accum_std = 0;
+
+        RoI(){}
+        RoI(int x, int y, int w, int h):x(x), y(y), w(w), h(h)
+        {
+            region_parts.emplace_back(x,y, w,h);
+        }
+
+        void appendRegion(const RoI& roi)
+        {
+            region_parts.insert(region_parts.end(), roi.region_parts.begin(), roi.region_parts.end());
+            accum_mean += roi.accum_mean;
+            accum_std += roi.accum_std;
+        }
+
+        void set(int x, int y, int w, int h)
+        {
+            this->x = x;
+            this->y = y;
+            this->w = w;
+            this->h = h;
+            region_parts.clear();
+            region_parts.emplace_back(x,y, w,h);
+        }
+
+        int getMean()
+        {
+            return accum_mean / region_parts.size();
+        }
+
+        int getStddev()
+        {
+            return accum_std / std::sqrt(region_parts.size());
+        }
+
+        cv::Rect toRect()
+        {
+            return cv::Rect(x, y, w, h);
+        }
+
+        bool isNeighbour( const RoI& roi)
+        {
+            for(const auto& rect1: region_parts)
+            {
+                for(const auto& rect2: roi.region_parts)
+                {
+                    int dx = rect2.x - rect1.x;
+                    int dy = rect2.y - rect1.y;
+                    
+                    if(dx == rect1.width ) // может оказаться соседом справа
+                    {
+                        if(rect1.y >= rect2.y && rect1.y <= rect2.y + rect2.height)
+                            return true;
+                        if(rect2.y >= rect1.y && rect2.y <= rect1.y + rect1.height)
+                            return true;
+                        else
+                            continue;
+                    } 
+                    
+                    if(dy == rect1.height) // может оказаться соседом снизу
+                    {
+                        if(rect1.x >= rect2.x && rect1.x <= rect2.x + rect2.width)
+                            return true;
+                        if(rect2.x >= rect1.x && rect2.x <= rect1.x + rect1.width)
+                            return true;
+                        else
+                            continue;
+                    }
+
+                    if(dx == -rect2.width) // может оказаться соседом слева
+                    {
+                        if(rect1.y >= rect2.y && rect1.y <= rect2.y + rect2.height)
+                            return true;
+                        if(rect2.y >= rect1.y && rect2.y <= rect1.y + rect1.height)
+                            return true;
+                        else
+                            continue;
+                    }
+
+                    if(dy == -rect2.height) // может оказаться соседом сверху
+                    {
+                        if(rect1.x >= rect2.x && rect1.x <= rect2.x + rect2.width)
+                            return true;
+                        if(rect2.x >= rect1.x && rect2.x <= rect1.x + rect1.width)
+                            return true;
+                        else
+                            continue;
+                    }
+                }
+            }
+            return false;
+        } 
+    };
+
+    int g_mean;
+    int g_stddev;
+    cv::Mat g_image;
+    std::vector<RoI> rois;
+    std::vector<bool> merged;
+
+    void split_image(RoI& roi)
+    {
+        cv::Mat mean;
+        cv::Mat dev;
+        cv::meanStdDev(g_image( roi.toRect()), mean, dev);
+
+        if (dev.at<double>(0) <= g_stddev || roi.w < 5 || roi.h < 5)
+        {
+            rois.emplace_back(roi.x, roi.y, roi.w, roi.h );
+            rois.back().accum_mean = mean.at<double>(0);
+            rois.back().accum_std = dev.at<double>(0);          
+            return;
+        }
+
+        int x, y, half_w, half_h;
+        x = roi.x;
+        y = roi.y;
+        half_h = roi.h/2;
+        half_w = roi.w/2;
+
+        roi.set(x, y, half_w, half_h);
+        split_image(roi);
+        roi.set(x + half_w, y, half_w, half_h);
+        split_image(roi);
+        roi.set(x + half_w, y + half_h, half_w, half_h);
+        split_image(roi);
+        roi.set(x, y + half_h, half_w, half_h);
+        split_image(roi);
+    } 
+
+    void merge_rois()
+    {
+        merged.clear();
+        merged.resize(rois.size(), false);
+
+        bool is_merge = true;
+        while (is_merge){
+            is_merge = false;
+            for(int i = 0; i < rois.size() - 1; i++)
+                if(!merged[i])
+                    for(int j = i+1; j < rois.size(); j++)
+                        if(!merged[j])
+                            if(rois[i].isNeighbour(rois[j]))
+                                if(std::abs(rois[i].getMean() - rois[j].getMean()) < g_mean &&
+                                   std::abs(rois[i].getStddev() - rois[j].getStddev()) < g_stddev)
+                                {
+                                    merged[j] = true;
+                                    rois[i].appendRegion(rois[j]);
+                                    is_merge = true;
+                                }                                   
+        }
     }
-
-    const auto width = image.cols;
-    const auto height = image.rows;
-
-    split_image(image(cv::Range(0, height / 2), cv::Range(0, width / 2)), stddev);
-    split_image(image(cv::Range(0, height / 2), cv::Range(width / 2, width)), stddev);
-    split_image(image(cv::Range(height / 2, height), cv::Range(width / 2, width)), stddev);
-    split_image(image(cv::Range(height / 2, height), cv::Range(0, width / 2)), stddev);
-}
-} // namespace
+} // nspace SaM
 
 namespace cvlib
 {
-cv::Mat split_and_merge(const cv::Mat& image, double stddev)
+cv::Mat split_and_merge(const cv::Mat& image, double stddev, int mean)
 {
-    // split part
-    cv::Mat res = image;
-    split_image(res, stddev);
+    SaM::rois.clear();
+    SaM::g_image = image;
+    SaM::g_stddev = stddev;
+    SaM::g_mean = mean;
 
-    // merge part
-    // \todo implement merge algorithm
-    return res;
+// split part
+    SaM::RoI roi(0, 0, image.cols, image.rows);
+    SaM::split_image(roi);
+        
+// merge part
+    SaM::merge_rois();
+    for(int i = 0; i < SaM::merged.size(); i++)
+        if(!SaM::merged[i])
+            for(const auto& rect: SaM::rois[i].region_parts)
+            {
+                SaM::g_image(rect).setTo(SaM::rois[i].getMean());
+            }
+    return SaM::g_image;
 }
+
 } // namespace cvlib
--- demo/demo_split_and_merge.cpp
+++ demo/demo_split_and_merge.cpp
@@ -5,12 +5,36 @@
  */
 
 #include <opencv2/opencv.hpp>
-
+#include <opencv2/highgui/highgui.hpp>
 #include <cvlib.hpp>
+#include <ctime>
+#include <string>
 
 int demo_split_and_merge(int argc, char* argv[])
 {
-    cv::VideoCapture cap(0);
+        const cv::String keys = // clang-format off
+      "{help h usage ? |      | print this message   }"
+      "{video          |      | video file           }";
+    // clang-format on
+    cv::CommandLineParser parser(argc, argv, keys);
+    parser.about("Application name v1.0.0");
+    if (parser.has("help"))
+    {
+        parser.printMessage();
+        return 0;
+    }
+
+    cv::VideoCapture cap;
+    if(parser.has("video"))
+    {
+        auto video = parser.get<cv::String>("video");
+        cap.open(video);
+    }else
+    {
+        cap.open(0);
+    }
+    
+
     if (!cap.isOpened())
         return -1;
 
@@ -20,18 +44,35 @@ int demo_split_and_merge(int argc, char* argv[])
     const auto origin_wnd = "origin";
     const auto demo_wnd = "demo";
 
-    int stddev = 50;
+    int stddev = 25, mean = 12;
     cv::namedWindow(demo_wnd, 1);
-    // \todo choose reasonable max value
-    cv::createTrackbar("stdev", demo_wnd, &stddev, 255);
+    cv::createTrackbar("stdev", demo_wnd, &stddev, 45);
+    cv::createTrackbar("mean", demo_wnd, &mean, 50);
+    int start, end;
+    float dur;
+
+    bool pause = false;
+
 
     while (cv::waitKey(30) != 27) // ESC
     {
-        cap >> frame;
+        if(!pause)
+        {
+            cap >> frame;
+            cv::resize(frame,frame,cv::Size(480,320));
+        }
+        if(cv::waitKey(30) == 112) pause = !pause;
+
+        cv::imshow(origin_wnd, frame);
 
         cv::cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
-        cv::imshow(origin_wnd, frame);
-        cv::imshow(demo_wnd, cvlib::split_and_merge(frame_gray, stddev));
+        cv::GaussianBlur(frame_gray, frame_gray, cv::Size(7,7), 0, 0);
+        start = clock();
+        frame_gray = cvlib::split_and_merge(frame_gray, stddev, mean);
+        end = clock();
+        dur = static_cast<float>((end - start))/CLOCKS_PER_SEC;
+        putText(frame_gray, std::string("fps: ") + std::to_string(static_cast<int>(1/dur)), cv::Point(5,50), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0), 2);
+        cv::imshow(demo_wnd, frame_gray);
     }
 
     cv::destroyWindow(origin_wnd);
