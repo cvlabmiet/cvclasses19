f1d9b2b9aacd646999155223848fae3d2bf065ef
Глеб Карпухин <gkarpukhin@gmail.com>
2023-01-19 09:40:07 +0300

    lab1 complete

--- demo/demo_split_and_merge.cpp
+++ demo/demo_split_and_merge.cpp
@@ -1,12 +1,5 @@
-/* Demo application for Computer Vision Library.
- * @file
- * @date 2018-09-05
- * @author Anonymous
- */
-
-#include <opencv2/opencv.hpp>
-
 #include <cvlib.hpp>
+#include <opencv2/opencv.hpp>
 
 int demo_split_and_merge(int argc, char* argv[])
 {
@@ -20,18 +13,21 @@ int demo_split_and_merge(int argc, char* argv[])
     const auto origin_wnd = "origin";
     const auto demo_wnd = "demo";
 
-    int stddev = 50;
+    int stddev = 10;
     cv::namedWindow(demo_wnd, 1);
-    // \todo choose reasonable max value
-    cv::createTrackbar("stdev", demo_wnd, &stddev, 255);
+    cv::createTrackbar("stdev", demo_wnd, &stddev, 50);
 
-    while (cv::waitKey(30) != 27) // ESC
+    while (cv::waitKey(30) != 27)
     {
         cap >> frame;
 
         cv::cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
         cv::imshow(origin_wnd, frame);
-        cv::imshow(demo_wnd, cvlib::split_and_merge(frame_gray, stddev));
+        cv::Mat merged = cvlib::split_and_merge(frame_gray, stddev);
+
+        if (!merged.empty())
+            cv::imshow(demo_wnd, cvlib::split_and_merge(frame_gray, stddev));
+        std::cout << "frame is ready" << std::endl;
     }
 
     cv::destroyWindow(origin_wnd);
